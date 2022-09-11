b81bf6505a1007e791e255ba77e308ab38aa12bb
lanlokun <mlanlokun@gmail.com>
2022-09-11 15:14:53 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+/*
+ * File : 5-print numbers.c
+ * Author : Malik K LANLOKUN
+ *
+*/
+
+#include <stdio.h>
+
+/**
+  * main - prints all single digit numbers of base 10 starting from 0
+  *
+  * Return: Always 0.
+*/
+
+int main(void)
+{
+	int n;
+
+	for  (n = 0; n < 10; n++)
+		printf("%d", n);
+
+	printf("\n");
+
+	return (0);
+}
+
+
