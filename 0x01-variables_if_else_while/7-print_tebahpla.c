324f119ad0bd625d2e7246890edb41856a14cf56
lanlokun <mlanlokun@gmail.com>
2022-09-11 15:38:28 -0700

    reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+/*
+ * File : 5-print numbers.c
+ * Author : Malik K LANLOKUN
+ *
+*/
+
+#include <stdio.h>
+/**
+  * main - prints all single digit numbers of base 10 starting from 0
+  *
+  * Return: Always 0.
+*/
+
+int main(void)
+{
+	char letter = 'z';
+
+	while(letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+
