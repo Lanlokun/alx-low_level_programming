5f06f28f6eb907821ce88042da129cb6159a394a
lanlokun <mlanlokun@gmail.com>
2022-09-11 15:50:22 -0700

    updates

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -18,7 +18,7 @@ int main(void)
 
 	for (n = 0; n < 10; n++)
 		putchar	((n % 10) + '0');
-	for(letter = 'a'; letter <= 'f'; letter++)
+	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
 
 	putchar('\n');
