5d718945c10af9e911d9d64de23edb0cc84b3bfd
lanlokun <mlanlokun@gmail.com>
2022-09-11 16:00:25 -0700

    possible combination of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+/*
+ * File : 5-print numbers.c
+ * Author : Malik K LANLOKUN
+ *
+*/
+
+#include <stdio.h>
+/**
+  * main - prints all single digit numbers of base 10 starting from 0
+  *
+  * Return: Always 0.
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar	((n % 10) + '0');
+		if(n == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
