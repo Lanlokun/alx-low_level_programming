a8ba1bc4ac018c0e0d9b1a248a1d9b07a3947023
lanlokun <mlanlokun@gmail.com>
2022-09-11 15:48:53 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+/*
+ * File : 5-print numbers.c
+ * Author : Malik K LANLOKUN
+ *
+*/
+
+#include <stdio.h>
+/**
+  * main - prints all single digit numbers of base 10 starting from 0
+  *
+  * Return: Always 0.
+*/
+
+int main(void)
+{
+	int n;
+	char letter;
+
+	for (n = 0; n < 10; n++)
+		putchar	((n % 10) + '0');
+	for(letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
+
+
