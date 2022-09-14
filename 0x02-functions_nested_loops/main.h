9c12f7a422253c69687f1f3cfe5aa3654b27f7b9
lanlokun <mlanlokun@gmail.com>
2022-09-14 09:45:29 -0700

    updates

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,11 @@
 #ifndef PUTCHAR_H
 #define PUTCHAR_H
+/*
+ *
+ * File: main.h
+ * Author:  Malik K Lanlokun
+ * Desc: Header file containing declarations for all functions
+ */
 
 int _putchar(char c);
 
