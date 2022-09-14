68ff5faf26f6d9fe8ad83c9e0376d6d17aea50a0
lanlokun <mlanlokun@gmail.com>
2022-09-14 14:04:45 -0700

    updates

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -14,12 +14,10 @@
  * Return 0
  */
 
-void print_alphabet()
+int main(void)
 {
-	int i;
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+	print_alphabet();
+
+	return (0);
+
 }
