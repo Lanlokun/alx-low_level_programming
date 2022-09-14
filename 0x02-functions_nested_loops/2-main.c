656eecc512a3327e4e5687d7f28626dbee00a53c
lanlokun <mlanlokun@gmail.com>
2022-09-14 14:26:19 -0700

    print lowercase alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,28 @@
+/**
+ *
+ * File : 1-alphabet.c
+ * Author : Malik K Lanlokun
+ *
+ */
+
+#include "main.h"
+
+/*
+ *
+ * main - print the alphabet in lowercase followed by a new line
+ *
+ * Return 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	while(i < 10)
+	{
+		print_alphabet();
+		i++;
+	}
+
+	return (0);
+
+}
