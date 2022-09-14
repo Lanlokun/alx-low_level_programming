d60471678892067027e957a6db68581cc3c53cc5
lanlokun <mlanlokun@gmail.com>
2022-09-14 14:12:47 -0700

    updates

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,7 @@
-/**
- *
- * File : 1-alphabet.c
- * Author : Malik K Lanlokun
- *
- */
-
 #include "main.h"
 
-/*
- *
+/**
  * print_alphabet - print the alphabet in lowercase followed by a new line
- *
  * Return 0
  */
 
