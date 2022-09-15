b1fd97f117376c955ed1c47ff276134ad8a8994e
lanlokun <mlanlokun@gmail.com>
2022-09-14 17:56:49 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,21 @@
+/**
+ *
+ * File : 6-abs.c
+ * Author : Malik K Lanlokun
+ *
+ */
+
+#include "main.h"
+
+/*
+ *
+ * times table checks for time
+ * @n: number to check
+ * Return: times table
+ */
+
+int main(void)
+{
+	times_table();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * 8-24 hours - checks for alphabets
+ * @n: character to check
+ * Return: every minute of the day of jack
+ *
+ */
+
+void times_table(void)
+{
+
+	int row;
+	int column;
+	int product;
+	for (row = 0; row <= 9;row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = (row * column);
+			if (column == 0)
+			{
+				_putchar('0' + product);
+			}
+			else if (product <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+			}
+			else if (product > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
