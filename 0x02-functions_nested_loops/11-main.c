e9c86e024178f2f3b67aada33b50bf980a7eb337
lanlokun <mlanlokun@gmail.com>
2022-09-14 18:33:30 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,27 @@
+/**
+ *
+ * File : 6-abs.c
+ * Author : Malik K Lanlokun
+ *
+ */
+
+#include "main.h"
+
+/*
+ *
+ * times table checks for time
+ * @n: number to check
+ * Return: times table
+ */
+
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+    	print_to_98(81);
+    	print_to_98(-10);
+	return(0);
+
+
+}
