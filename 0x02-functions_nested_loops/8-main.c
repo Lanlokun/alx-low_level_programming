947f4e58bd1815eb523151a82a79aa55ce6667e1
lanlokun <mlanlokun@gmail.com>
2022-09-14 17:42:16 -0700

    print 24 hrs

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,21 @@
+/**
+ *
+ * File : 6-abs.c
+ * Author : Malik K Lanlokun
+ *
+ */
+
+#include "main.h"
+
+/*
+ *
+ * print_last_digit - checks for sign
+ * @n: number to check
+ * Return: last digit of number
+ */
+
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
