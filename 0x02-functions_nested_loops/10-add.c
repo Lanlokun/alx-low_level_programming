2f276f5423c797a0fdcab101db6ffdc2009461a9
lanlokun <mlanlokun@gmail.com>
2022-09-14 18:12:57 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - checks for alphabets
+ * @n: character to check
+ * Return: absolute last digit of a number
+ *
+ */
+
+int add(int a, int b)
+{
+
+	int result = a + b;
+
+	return result;
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,26 @@
+/**
+ *
+ * File : 6-abs.c
+ * Author : Malik K Lanlokun
+ *
+ */
+
+#include "main.h"
+#include <stdio.h>
+/*
+ *
+ * add - add two numbers
+ * @a: two integer arguments
+ * @b: two integer arguments
+ * Return: sum
+ */
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+
+    	return (0);
+}
