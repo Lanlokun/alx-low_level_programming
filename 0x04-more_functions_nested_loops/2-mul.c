e0a09d11d642515382ae2b7818ea0ed55f198564
lanlokun <mlanlokun@gmail.com>
2022-09-16 13:50:29 -0700

    collaboration is multiplicattion

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if value is a digit
+ * @c: character  to check
+ * Return: 1 if digit, 0 if not digit
+ *
+ */
+int mul(int a, int b)
+{
+	return a * b;
+	_putchar('\n');
+
+}
