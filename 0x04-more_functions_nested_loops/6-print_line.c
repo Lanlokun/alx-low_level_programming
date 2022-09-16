70f5e91a62a8c3378681b24ec60a8a256e24ee83
lanlokun <mlanlokun@gmail.com>
2022-09-16 15:17:13 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - print line
+ * @a: value to multiply
+ * @b: value to multiply
+ * Return: result of multiplying a and b
+ *
+ */
+void print_line(int n)
+{
+
+	int i = 0;
+	while(i < n)
+	{
+
+		_putchar('_');
+		i++;
+	}
+
+	_putchar('\n');
+
+}
