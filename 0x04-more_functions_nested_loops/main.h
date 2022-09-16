ee3e0ae4e3be82f9065dd5bfc27054fdc0153d07
lanlokun <mlanlokun@gmail.com>
2022-09-16 13:17:22 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef PUTCHAR_H
+#define PUTCHAR_H
+/*
+ *
+ * File: main.h
+ * Author:  Malik K Lanlokun
+ * Desc: Header file containing declarations for all functions
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
