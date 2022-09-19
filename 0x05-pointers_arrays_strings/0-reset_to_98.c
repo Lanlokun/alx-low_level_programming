190551137447a3f81781ca3d40d433e127d59dfe
lanlokun <mlanlokun@gmail.com>
2022-09-19 15:44:29 -0700

    battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+/**
+ * reset - function that dereferences
+ *
+ *
+ *
+*/
+
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ *
+ * File: main.h
+ * Author:  Malik K Lanlokun
+ * Desc: Header file containing declarations for all functions
+ */
+
+void reset_to_98(int *n);
+
+#endif
