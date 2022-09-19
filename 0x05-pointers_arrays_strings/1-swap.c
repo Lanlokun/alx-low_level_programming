dc745c780f1bbb6f673ef2412cb9ff8afe27679b
lanlokun <mlanlokun@gmail.com>
2022-09-19 16:27:59 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,19 +1,20 @@
 /**
  * File: 0-reset_to_98.c
  * Auth: Malik K Lanlokun
-*/
+ */
 
 #include "main.h"
 /**
- * _swap_int - swaps the values of two integers. 
- *
+ * swap_int - swaps the values of two integers.
  * @a: variable to swap
  * @b: variable to swap
  *
  */
 
-void swap_int(int *a, int *b) {
+void swap_int(int *a, int *b) 
+{
 	int c;
+
 	c = *a;
 	*a = *b;
 	*b = c;
