6fbd734a5ab2a64b9bc4ce6570d1a4db7de84171
lanlokun <mlanlokun@gmail.com>
2022-09-19 15:57:24 -0700

    betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,7 +5,8 @@
 
 #include "main.h"
 /**
-  * 0-reset - takes a pointer to an int as parameter and updates the value it points to to 98
+  * reset_to_98 - takes a pointer to an int as parameter and updates the value
+  * it points to to 98
   *
   * @n: pointer parameter
   */
