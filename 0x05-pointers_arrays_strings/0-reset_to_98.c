c98afd20e39793b411a9e6b8c39df63ac34f1ce9
lanlokun <mlanlokun@gmail.com>
2022-09-19 15:53:55 -0700

    betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,14 @@
 /**
- * reset - function that dereferences
- *
- *
- *
+ * File: 0-reset_to_98.c
+ * Auth: Malik K Lanlokun
 */
 
+#include "main.h"
+/**
+  * 0-reset - takes a pointer to an int as parameter and updates the value it points to to 98
+  *
+  * @n: pointer parameter
+  */
 
 void reset_to_98(int *n)
 {
