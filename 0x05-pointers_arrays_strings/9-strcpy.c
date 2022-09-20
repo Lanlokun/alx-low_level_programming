8ac7484f625b0b443502329a9c0b247a20dc3500
lanlokun <mlanlokun@gmail.com>
2022-09-20 08:24:53 -0700

    string copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -16,13 +16,13 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while(src[index])
+	while (src[index])
 	{
 		dest[index] = src[index];
 		index++;
 	}
 
-	return(dest);
+	return (dest);
 
 }
 
