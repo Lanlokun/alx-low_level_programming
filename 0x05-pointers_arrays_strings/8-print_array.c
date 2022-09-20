69dbcbc78306fd18e3018c9276939b95b350317d
lanlokun <mlanlokun@gmail.com>
2022-09-20 06:33:20 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+/**
+ * File: 8-print_array.c
+ * Auth: Malik K Lanlokun
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print array - Prints half of a string
+ * @s: the string to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%d", a[i]);
+		if (i == n - 1)
+			continue;
+		printf(", ");
+	}
+
+	printf("\n");
+
+}
+
