2dcffdf2feefd24930968979831d933ec1a8c05d
lanlokun <mlanlokun@gmail.com>
2022-09-20 08:22:02 -0700

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+/**
+ * File: 9-strcpy.c
+ * Auth: Malik K Lanlokun
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies value of a pinter reference to another pointer
+ * @dest : destination
+ * @src : source
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while(src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return(dest);
+
+}
+
