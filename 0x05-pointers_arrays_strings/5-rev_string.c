56dd77c51f44b74908e5d33e88f95459f364414a
lanlokun <mlanlokun@gmail.com>
2022-09-20 05:16:14 -0700

    string reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+/**
+ * File: 0-reset_to_98.c
+ * Auth: Malik K Lanlokun
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - swaps the values of two integers.
+ * @s: the string to be printed
+ */
+
+void rev_string	(char *s)
+{
+	int len = 0, index = 0;
+	char tmp;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= len / 2; index--)
+	{
+
+		tmp = s[index];
+
+		s[index] = s[len - index - 1];
+
+		s[len - index - 1] = tmp;
+	}
+}
