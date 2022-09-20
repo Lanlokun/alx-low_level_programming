fd412b36216655533cece0fb0e5f9244f1e96de5
lanlokun <mlanlokun@gmail.com>
2022-09-20 06:10:50 -0700

    updates

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+/**
+ * File: 6-puts2.c
+ * Auth: Malik K Lanlokun
+ */
+
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @s: the string to be printed
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int index = 0, n;
+
+	while(str[index++])
+		len++;
+
+	if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (index = n; index < len; index++)
+		_putchar(str[index]);
+
+	_putchar('\n');
+
+}
+
