140751e548f2ed2579433808cd21cfef8b17f9f8
lanlokun <mlanlokun@gmail.com>
2022-09-21 07:56:16 -0700

    concatenation

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+#C Programming 
+# more pointers and functions
+
+Task 0 | string concatenate function
+
+
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ *
+ * File: main.h
+ * Author:  Malik K Lanlokun
+ * Desc: Header file containing declarations for all functions
+ */
+
+char *_strcat(char *dest, char *src);
+
+#endif
