83b3776ef1d450e919176157eaf4cd47cc85bac0
lanlokun <mlanlokun@gmail.com>
2022-09-27 16:06:07 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,30 @@
+/*
+ * File: 7-print_chessboard.c
+ * Auth: Malik K Lanlokun
+ */
+
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chessboard .
+ * @a - array pointer
+ *
+ * Return: 
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+
+	int i, j;
+
+	for (i = 0 ; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+
+		}
+		_putchar('\n');
+	}
+}
