8f71955a383ecf0cbc43b34596225d46051c1b62
lanlokun <mlanlokun@gmail.com>
2022-10-05 04:11:05 -0700

    arg count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - argument count.
+ * @argc -
+ * @argv -
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
