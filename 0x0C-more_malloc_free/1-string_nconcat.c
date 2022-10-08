d0f8f08511c1d5304ed4e39bf0f3d2a8f14cf569
lanlokun <mlanlokun@gmail.com>
2022-10-08 06:21:33 -0700

    sring concat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+/*
+ *File: 0-malloc_checked.c
+ *Auth: Malik K Lanlokun
+ *    
+ *    
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - Allocates memory using malloc.
+ * @s1: pointer to first string.
+ * @s2: pointer to second string.
+ *
+ * Return: A pointer to a newly allocated space in memory, which contains s1, followed by the first n bytes of s2, and null terminated
+ * 
+ */
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n) { char *concat; 
+	unsigned int len = n, index; 
+
+	if (s1 == NULL)
+		s1 = ""; 
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		len++; 
+	
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
+		return (NULL); 
+	len = 0; 
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index]; 
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index]; 
+
+	concat[len] = '\0'; 
+
+	return (concat);
+}
