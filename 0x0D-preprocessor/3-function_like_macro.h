a45ab4cfdf66628ec5d634e1cf5052f7b4a969ba
lanlokun <mlanlokun@gmail.com>
2022-10-10 14:15:31 -0700

    absolute value

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H 
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * File: 3-function_like_macro.h
+ * Auth: Malik K Lanlokun
+ * Desc: Header file that defines a macro function that prints the absolute value fo a number
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
