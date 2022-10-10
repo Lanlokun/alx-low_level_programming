5767486f9c708677f651bf91c968e2a035baac2f
lanlokun <mlanlokun@gmail.com>
2022-10-10 14:19:55 -0700

    code updates

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H 
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Auth: Malik K Lanlokun
+ * Desc: Header file that defines a macro function that prints th sum of x and y
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
