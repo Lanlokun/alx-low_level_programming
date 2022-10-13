40d97e027c992d0e7e942a5023ae32c81e7190f5
lanlokun <mlanlokun@gmail.com>
2022-10-13 11:18:21 -0700

    function pointer print names

---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ *
+ * File: main.h
+ * Author:  Malik K Lanlokun
+ * Desc: Header file containing declarations for all functions
+ */
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
