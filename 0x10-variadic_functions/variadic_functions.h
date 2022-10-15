9ab770bf56596da040badb4a12fc4e7375862bcd
lanlokun <mlanlokun@gmail.com>
2022-10-14 17:23:12 -0700

    sum of variadic args

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ *
+ * File: main.h
+ * Author:  Malik K Lanlokun
+ * Desc: Header file containing declarations for all functions
+ *
+ */
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
